c6e7b45b596d8f4cf139466822db78a08092972d
ProgrammingMuffin <deepakcoder98@gmail.com>
2018-12-30 13:31:13 +0000

    updated all files

---
+++ manipulate.h
@@ -0,0 +1,10 @@
+#ifndef MANIPULATE_H
+#define MANIPULATE_H
+
+#include<iostream>
+
+std::string GetUnique(std::string str);
+void GetCount(std::string in_str, int count[], std::string temp);
+void ClearPool();
+
+#endif
