1a88a3ebd19c8f3688ff6f8b056198fae3e0d332
ProgrammingMuffin <deepakcoder98@gmail.com>
2018-12-30 10:23:09 +0000

    Added 2 functions for initalizations

---
+++ compress.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include "compress.h"
+
+NODE LEAF[128];
+NODE NODE_POOL[256];
+NODE DELETED[256];
+int LEAF_TOP = 0;
+int POOL_TOP = 0;
+int DEL_TOP = 0;
+
+void InitLeaves(std::string input, int count[]) 
+{
+  int i;
+  for(i=0;i<(int)input.length();i++)
+  {
+    LEAF[i].current = input[i];
+    LEAF[i].freq = count[i];
+  }
+  LEAF_TOP = i;
+}
+
+void InjectPool() //based on deleted nodes and leaf[] it injects
+{
+  int t = LEAF_TOP;
+  while(t != 0)
+  {
+    NODE_POOL[POOL_TOP] = LEAF[POOL_TOP];
+    POOL_TOP++;
+    t--;
+  }
+  std::cout<<"Injected NODE_POOL"<<std::endl;
+}
+
+void PrintLeaves(NODE input[], int n)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    std::cout<<"Leaf "<<i+1<<": "<<input[i].current<<", "<<input[i].freq<<std::endl;
+  }
+}
